acd0f8de69118cf0744ab9bf295a91903592d4ce
luyi404 <370977893@qq.com>
2023-03-01 17:42:55 -0500

    [ftr] Implement Breakdown scene

--- src/scenes/overview/index.jsx
+++ src/scenes/overview/index.jsx
@@ -9,7 +9,7 @@ const Overview = () => {
 
     return (
         <Box m={"1.5rem 2.5rem"}>
-            <Header title={"OVERVIEW"} subtitle={"Overview of general revenue and profit"}/>
+            <Header title={"OVERVIEW"} subtitle={"overview of general revenue and profit"}/>
             <Box
                 height={"75vh"}
             >
